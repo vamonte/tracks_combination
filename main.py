8c12599bba546e689b64994fd4b10ab3c0f2b144
Valentin Monté <valentin.monte@yahoo.fr>
2022-03-16 18:09:26 +0100

    feat(check_tracks_length): Improve the check_tracks_length function to allow a "delta" argument The delta argument is usefull to allow tolerance in the check process

--- main.py
+++ main.py
@@ -4,23 +4,35 @@ from typing import List, Optional, Tuple
 def validate_tracks_length(
     tracks_length: Tuple[int],
     indices: List[int],
-    total_length: int,
+    min_length: int,
+    max_length: int,
     tracks_count: int,
 ):
     tracks_list = tuple(tracks_length[i] for i in indices)
-    if sum(tracks_list) == total_length and tracks_count == len(tracks_list):
+    length = sum(tracks_list)
+    if (
+        length >= min_length
+        and length <= max_length
+        and tracks_count == len(tracks_list)
+    ):
         return True
     return False
 
 
 def check_tracks_length(
-    tracks_length: List[int], concert_premiere_length: int, tracks_count: int = 3
+    tracks_length: List[int],
+    concert_premiere_length: int,
+    tracks_count: int = 3,
+    delta: int = 0,
 ):
     if len(tracks_length) < tracks_count:
         return False
 
+    min_length = concert_premiere_length - delta
+    max_length = concert_premiere_length + delta
+
     valid_tracks_length = tuple(
-        length for length in tracks_length if length < concert_premiere_length
+        length for length in tracks_length if length < max_length
     )
 
     track_length_size = len(valid_tracks_length)
@@ -29,7 +41,7 @@ def check_tracks_length(
 
     indices = list(range(tracks_count))
     if validate_tracks_length(
-        valid_tracks_length, indices, concert_premiere_length, tracks_count
+        valid_tracks_length, indices, min_length, max_length, tracks_count
     ):
         return True
 
@@ -44,7 +56,7 @@ def check_tracks_length(
             indices[j] = indices[j - 1] + 1
 
         if validate_tracks_length(
-            valid_tracks_length, indices, concert_premiere_length, tracks_count
+            valid_tracks_length, indices, min_length, max_length, tracks_count
         ):
             return True
 
@@ -53,4 +65,6 @@ if __name__ == "__main__":
     assert check_tracks_length([2, 3], 5) is False
     assert check_tracks_length([1, 2, 6], 6) is False
     assert check_tracks_length([1, 2, 3], 6) is True
+    assert check_tracks_length([1, 2, 3], 8) is False
+    assert check_tracks_length([1, 2, 3], 6, delta=2) is True
     assert check_tracks_length([1, 2, 3, 4, 30, 20], 54) is True
